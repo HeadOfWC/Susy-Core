3ae68e81844f7da7e4b9600dac9da4f001306d3b
HeadOfWC <123336769+HeadOfWC@users.noreply.github.com>
2024-01-24 09:15:11 -0500

    Update MetaTileEntityGeothermalPump.java

--- src/main/java/supersymmetry/common/metatileentities/multi/electric/MetaTileEntityGeothermalPump.java
+++ src/main/java/supersymmetry/common/metatileentities/multi/electric/MetaTileEntityGeothermalPump.java
@@ -1,1 +1,29 @@
+package supersymmetry.common.metatileentities.multi.electric;
 
+import gregtech.api.metatileentity.MetaTileEntity;
+import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
+import gregtech.api.metatileentity.multiblock.IMultiblockPart;
+import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
+import gregtech.api.pattern.BlockPattern;
+import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.api.unification.material.Materials;
+import gregtech.client.renderer.ICubeRenderer;
+import gregtech.client.renderer.texture.Textures;
+import gregtech.common.blocks.MetaBlocks;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.ResourceLocation;
+import supersymmetry.api.recipes.SuSyRecipeMaps;
+import gregtech.common.blocks.BlockBoilerCasing.BoilerCasingType;
+import gregtech.common.blocks.BlockMetalCasing.MetalCasingType;
+import supersymmetry.api.capability.impl.NoEnergyMultiblockRecipeLogic;
+import supersymmetry.client.renderer.textures.SusyTextures;
+
+import javax.annotation.Nonnull;
+
+public class MetaTileEntityGeothermalPump RecipeMapMultiblockController {
+
+    public MetaTileEntityGeothermalPump(ResourceLocation metaTileEntityId) {
+        super(metaTileEntityId, SuSyRecipeMaps.GEOTHERMAL_PUMP_RECIPES);
+        
+    }
+}
